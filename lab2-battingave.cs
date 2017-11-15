e6749866876f553319b908bf1870402c1bcd8c9b
Jacobi <smartflaguy813@gmail.com>
2017-11-14 18:41:59 -0600

    Add project files.

---
+++ lab2-battingave.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Project: Lab 2 batting average
+ *    11-13-2017
+ *    Author: J Guyton 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_battingav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //variable declaration
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAverage;
+
+            //prompt user
+
+            Console.WriteLine("Calculate Batting Average");
+                Console.WriteLine("Enter The Player's name: ");
+            strPlayerName = Console.ReadLine();
+
+            //prompt user for hits 
+            Console.WriteLine("Enter the player's # of hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+
+            //prompt user for number of at bats
+            Console.WriteLine("Enter the player's # of at bats:");
+            nAtBats = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            dblBattingAverage = (double)nHits / nAtBats;
+
+            //display results
+            Console.WriteLine(strPlayerName + "'s batting average is " + dblBattingAverage);
+            Console.ReadLine();
+
+
+
+
+ 
+        }
+    }
+}
