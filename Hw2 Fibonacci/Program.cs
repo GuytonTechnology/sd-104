52b8c4b81d0b235cad02135f8411e1632dc25639
GuytonTechnology <33643898+GuytonTechnology@users.noreply.github.com>
2017-11-20 22:01:44 -0600

    Add files via upload

---
+++ Hw2 Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2_Fibonacci
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+            int b = 2;
+            Console.WriteLine("which Fibonacci number");
+            int FibNumber = Convert.ToInt32(Console.ReadLine());
+            // n represents fibonacci number
+            for (int i = 1; i < FibNumber; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+           
+        
+        }
+    }
+}
